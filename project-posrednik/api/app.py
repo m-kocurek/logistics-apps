c88d6fa6bf5ac2865a33f1ff9b35da4143225ab5
mkocurek7 <58214646+mkocurek7@users.noreply.github.com>
2021-04-05 18:45:05 +0200

    Dodanie elementow wizualizacji wymagan (2 tabele, wylistowanie calk. kosztow, zysku ect), pobranie danych z pliku app.py i wyswietlenie na stronie.)

--- project-posrednik/api/app.py
+++ project-posrednik/api/app.py
@@ -7,21 +7,25 @@ app.config["DEBUG"]=True
 #webowa, bez interfejsu(bez formularzy do wpisania danych), z wizualizacja danych (wyswietlanie na stronie)
 
 
-array =[{"onecolumn": 000 , "2ndcolumn": 111},
-{"onecolumn": 000 , "2ndcolumn": 111},
-{"onecolumn": 000 , "2ndcolumn": 111}]
-
-
 @app.route('/home', methods=['GET'])
 def start():
-   # return 'hello '  
     return render_template("index.html")
 
 
 @app.route('/posrednik', methods=['GET'])
 def posrednik():
     #moze wywolanie metody obliczeniowej? 
-    return render_template("posrednik.html", array=array)
+    #1 tabela zmienna
+    rows=222
+    zmienna=333
+    #tabela 2 zmienne
+
+    #napisy zmienne
+    zysk_posrd=0
+    calk_koszt=1
+    calk_przychod=2
+    return render_template("posrednik.html",rows=rows, zmienna=zmienna,
+     calk_przychod=calk_przychod, zysk_posrd= zysk_posrd  , calk_koszt=calk_koszt)
 
 
 def logistics_method():
