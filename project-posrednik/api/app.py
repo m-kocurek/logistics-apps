5339e1f325baf88abdd4571c3aa2d6e5ea3c31a0
mkocurek7 <58214646+mkocurek7@users.noreply.github.com>
2021-04-19 01:45:21 +0200

    Update app.py
    przy odpalaniu warto mieć pliki z algorytmami w tym samym katalogu

--- project-posrednik/api/app.py
+++ project-posrednik/api/app.py
@@ -1,11 +1,9 @@
 import flask as Flask
 from flask import *
+import main
 
 app = Flask(__name__)
 app.config["DEBUG"]=True
-#info: 4 dostawcow, 2 odbiorcow, z blokowaniem tras, export danych do txt?
-#webowa, bez interfejsu(bez formularzy do wpisania danych), z wizualizacja danych (wyswietlanie na stronie)
-
 
 @app.route('/home', methods=['GET'])
 def start():
@@ -14,18 +12,22 @@ def start():
 
 @app.route('/posrednik', methods=['GET'])
 def posrednik():
-    #moze wywolanie metody obliczeniowej? 
-    #1 tabela zmienna
-    rows=222
-    zmienna=333
-    #tabela 2 zmienne
+    #Tabela zysków jednostkowych 
+    kt = [[0, 0,0], [0, 0,0], [0, 0,0], [0,0,0],[0,0,0]]
+    #Tabela optymalnych przewozów
+    tab_opt=[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 
-    #napisy zmienne
     zysk_posrd=0
-    calk_koszt=1
-    calk_przychod=2
-    return render_template("posrednik.html",rows=rows, zmienna=zmienna,
-     calk_przychod=calk_przychod, zysk_posrd= zysk_posrd  , calk_koszt=calk_koszt)
+    calk_koszt=0
+    calk_przychod=0
+    kt, tab_opt, zysk_posrd ,calk_koszt, calk_przychod =main_.main()
+
+    return render_template("posrednik.html",
+     calk_przychod=calk_przychod,
+      zysk_posrd= zysk_posrd, 
+      calk_koszt=calk_koszt,
+      kt=kt,
+      tab_opt=tab_opt)
 
 
 def logistics_method():
