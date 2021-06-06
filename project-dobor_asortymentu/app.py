92807999bea2538d880fbe9149bcb10e35a09c6d
mkocurek7 <58214646+mkocurek7@users.noreply.github.com>
2021-06-06 18:02:36 +0200

    dodanie działajacej aplikacji XDDDDD

---
+++ project-dobor_asortymentu/app.py
@@ -0,0 +1,256 @@
+import flask as Flask
+from flask import *
+from flask import session
+import sieci
+from sieci import sieci_
+import flask
+from flask import Flask,request, jsonify, render_template, redirect, url_for, session
+import sys
+import os
+
+app = Flask(__name__)
+app.config["DEBUG"]=True
+
+@app.route('/home', methods=['GET'])
+def start():
+    return render_template("index.html")
+
+
+@app.route('/posrednik', methods=['GET'])
+def posrednik():
+    #Tabela zysków jednostkowych 
+    kt = [[0, 0,0], [0, 0,0], [0, 0,0], [0,0,0],[0,0,0]]
+    #Tabela optymalnych przewozów
+    tab_opt=[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+
+    zysk_posrd=0
+    calk_koszt=0
+    calk_przychod=0
+
+    return render_template("posrednik.html",
+     calk_przychod=calk_przychod,
+     zysk_posrd= zysk_posrd, 
+      calk_koszt=calk_koszt,
+      kt=kt,
+      tab_opt=tab_opt )
+
+
+@app.route('/sieci', methods=['GET', 'POST'])
+def sieci():
+    if request.method == 'GET':
+        return render_template('sieci.html')
+    else:
+
+        liczba_wyrobow = str(request.form['liczba_wyrobow'])
+        session['liczba_wyrobow'] = liczba_wyrobow
+        if(liczba_wyrobow != 0):
+           with open('baza.txt', 'a') as plik:
+                plik.write(liczba_wyrobow)
+                plik.write("\n")
+               # plik.close()
+
+        cena = str(request.form['wyrob_cena'])
+        if(cena !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(cena)
+                plik.write("\n")
+               # plik.close() 
+       
+        liczba_surowcow = str(request.form['liczba_surowcow'])
+        if(liczba_surowcow !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(liczba_surowcow)
+                plik.write("\n")
+               # plik.close() 
+        session['liczba_surowcow']=liczba_surowcow
+
+        liczba_ograniczen = str(request.form['liczba_ograniczen'])
+        if(liczba_ograniczen !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(liczba_ograniczen)
+                plik.write("\n")
+               # plik.close() 
+       
+
+        x1 = str(request.form['x1'])
+        if(x1 !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(x1)
+                plik.write("\t")
+                plik.close() 
+
+        x2 = str(request.form['x2'])
+        if(x2 !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(x2)
+                plik.write("\t")
+                plik.close() 
+        x3 = str(request.form['x3'])
+        if(x3 !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(x3)
+                plik.write("\t")
+                plik.close() 
+        x4 = str(request.form['x4'])
+        if(x4 !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(x4)
+                plik.write("\t")
+                plik.close() 
+        znka = str(request.form['znak'])
+        if(znka !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(znka)
+                plik.write("\t")
+                plik.close() 
+        pr_strona = str(request.form['pr_strona'])
+        if(pr_strona !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(pr_strona)
+                plik.write("\t")
+                plik.close() 
+                   
+       # with open('baza.txt', 'a') as plik:
+            #for linia in plik:
+             #print (linia.strip().split())
+
+        #plik = open('baza.txt','w')
+       #    plik.write(liczba_wyrobow)
+        #   plik.write("\n")
+      #  plik.close()
+
+       # dlg_listy =len(lista_D_id)
+    zmienna ="abc"
+    return render_template("sieci.html", zmienna=zmienna)
+
+######### LICZBA WYROBOW
+
+@app.route('/wpiszLW', methods=['GET','POST'])
+def wpiszLW():
+    if request.method == 'GET':
+        zmienna="Wpisz liczbe wyrobow"
+        return render_template('wpiszLW.html',zmienna=zmienna)
+    else:    
+        liczba_wyrobow = str(request.form['liczba'])
+        session['liczba_wyrobow'] = liczba_wyrobow
+        if(liczba_wyrobow != 0):
+           with open('baza.txt', 'a') as plik:
+                plik.write(liczba_wyrobow)
+                plik.write("\n")
+               # plik.close()
+                 
+    zmienna="wpisz liczbe wyrobow"
+    return render_template("wpiszLW.html", zmienna=zmienna)
+
+#CENA WYROBOW
+@app.route('/wpiszCW', methods=['GET','POST'])
+def wpiszCW():
+    if request.method == 'GET':
+        zmienna="Wpisz cenę wyrobów"
+        return render_template('wpiszCW.html', zmienna=zmienna)
+    else:    
+        cena = str(request.form['liczba'])
+        if(cena !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(cena)
+                plik.write("\n")
+               # plik.close() 
+    zmienna="Wpisz cenę wyrobów"
+    return render_template("wpiszCW.html", zmienna=zmienna)
+
+#LICZBA SUROWCOW
+@app.route('/wpiszLS', methods=['GET','POST'])
+def wpiszLS():
+    if request.method == 'GET':
+        zmienna="Wpisz liczbę surowców " 
+        return render_template('wpiszLS.html', zmienna=zmienna)
+    else:    
+        cena = str(request.form['liczba'])
+        if(cena !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(cena)
+                plik.write("\n")
+               # plik.close() 
+                 
+    zmienna="Wpisz liczbę surowców " 
+    return render_template("wpiszLS.html", zmienna=zmienna)
+
+#wpisz LICZBE OGRANICZEN 
+@app.route('/wpiszLOgr', methods=['GET','POST'])
+def wpiszLOgr():
+    if request.method == 'GET':
+        zmienna="Wpisz liczbę ograniczeń" 
+        return render_template('wpiszLOgr.html')
+    else:
+        cena = str(request.form['liczba'])
+        if(cena !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(cena)
+                plik.write("\n")
+               # plik.close() 
+                 
+    zmienna="Wpisz liczbę ograniczeń " 
+    return render_template("wpiszLOgr.html", zmienna=zmienna)
+
+
+#wpisz ograniczenia
+@app.route('/wpiszOgraniczenie', methods=['GET','POST'])
+def wpiszOgraniczenie():
+    if request.method == 'GET':
+        return render_template('wpisz_ogr.html')
+    else:
+        
+        x1 = str(request.form['x1'])
+        if(x1 !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(x1)
+                plik.write(":")
+                plik.close() 
+
+        x2 = str(request.form['x2'])
+        if(x2 !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(x2)
+                plik.write(":")
+                plik.close() 
+        x3 = str(request.form['x3'])
+        if(x3 !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(x3)
+                plik.write(":")
+                plik.close() 
+        x4 = str(request.form['x4'])
+        if(x4 !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(x4)
+                plik.write(":")
+                plik.close() 
+        znka = str(request.form['znak'])
+        if(znka !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(znka)
+                plik.write(":")
+                plik.close() 
+        pr_strona = str(request.form['pr_strona'])
+        if(pr_strona !=0):
+            with open('baza.txt', 'a') as plik:
+                plik.write(pr_strona)
+                plik.write("\n")
+                plik.close() 
+
+    zmienna="Wpisz ograniczenia"
+    return render_template("wpisz_ogr.html", zmienna=zmienna)
+
+
+#############################################################################
+
+@app.route('/sieci-wyniki', methods=['GET'])
+def PokazWyniki():
+    zysk, wynik_opt, wspolczynniki = sieci_()
+
+    #wspolczynniki =  wspol[0] 
+    return render_template("wyniki.html", zysk=zysk, wynik_opt=wynik_opt , wspolczynniki=wspolczynniki)
+    
+app.secret_key = " tr3yg4iwyhbfskcb74yir3hufw"
+app.run()
+
---
+++ project-dobor_asortymentu/sieci.py
@@ -0,0 +1,125 @@
+from scipy.optimize import linprog
+import linecache as lc
+import pandas as pd
+
+def sieci_():
+      #TODO:
+      #- ZROBIC Z TEGO FUNKCJE
+      #OBLICZONE ARG PRZEKAZAC DO FUNCKJI W APP.PY
+      # WYSWIETLIC WYNIKI
+      #ZABIEGI UPIEKSZAJACE
+      #zmien czyszczenie pliku
+
+
+######## ODCZYT Z PLIKU #########################
+
+      df=pd.read_csv('baza.txt')
+      dane = pd.DataFrame(data=df, columns=['A'] )
+
+############## LICZBA WYROBOW
+      Liczba_wyrobow = int(df.iat[0,0])  
+      print("liczba wyrobow")
+      print(Liczba_wyrobow)
+      Cena_wyrobow=[0 for x in range(Liczba_wyrobow)]
+                    
+      lw=Liczba_wyrobow               #np 2   
+      obj = [0 for x in range(lw)]
+
+############## CENY WYROBOW
+      for i in range(Liczba_wyrobow):
+            Cena_wyrobow[i] = float(df.iat[1+i,0]) 
+      print("ceny wyrobow")
+      print(Cena_wyrobow)
+         
+      for i in range(lw):
+            obj[i] = Cena_wyrobow[i]   #np 1 i 1
+
+############# LICZBA SUROWCOW 
+      Liczba_surowcow = int(df.iat[Liczba_wyrobow+1,0]) 
+      print("liczba surowcow")
+      print(Liczba_surowcow)
+             
+      ls=Liczba_surowcow                  #np 2
+
+############ LICZBA OGRANICZEN 
+      Liczba_Ograniczen= int (df.iat[Liczba_wyrobow +2,0]) 
+      print("liczba ograniczen")
+      print(Liczba_Ograniczen)
+
+#petla for do  pobrania i podzielenia rownania
+      for i in range(Liczba_Ograniczen):
+            Ograniczenia=df.iat[Liczba_wyrobow+3+i,0]
+            split_tab_ogr=Ograniczenia.split(':')
+            print("znak")
+            print(split_tab_ogr[-2])
+            print("prawa strona rownania") 
+            print(split_tab_ogr[-1])
+            print("same wspolczynniki!") 
+            print(split_tab_ogr[:-2]) #wyszly tylko wspolczynniki bez znaku i PRAWEJ strony rownania
+      
+      open("baza.txt", "w").close() #wyczysc plik
+      open("baza.txt", "w").write(". \n") #wrzuc krope i dawaj dalej XD
+
+###############################################
+#po wprowadzeniu przykladowych 
+# liczb funkcja celu wyglada tak:
+#funkcja celu
+# z = x1 + x2 -> max
+#wartosci domyslne, w przypadku gdy uzytkownik nie poda ograniczen
+#dla ograniczenia <=
+      ls_eql = [[0 for x in range(lw)]]#lew strona ograniczen <=
+      rs_eql = [0] #prawa strona oograniczen <=
+#dla ograniczenia >=, wszystko należy przemnożyć przez -1
+#dla ograniczenia =
+      ls_eq = [[0 for x in range(lw)]]
+      rs_eq = [0]
+#przykladowe ograniczenia
+# 6x1+6x2 <= 36000
+# 10x1 + 5x2 <= 50000
+# x2 <= 4000
+
+      for i in range(Liczba_Ograniczen):
+            Ograniczenia=df.iat[Liczba_wyrobow+3+i,0]
+            split_tab_ogr=Ograniczenia.split(':')
+            print("znak")
+            print(split_tab_ogr[-2])
+            znak= split_tab_ogr[-2]
+
+            print("prawa strona rownania") 
+            print(split_tab_ogr[-1])
+            right= float(split_tab_ogr[-1] )
+
+            print("\nlewa strona rownania:")
+            print(split_tab_ogr[:-2]) 
+            left = [0 for x in range(lw)]
+            wspolczynniki = split_tab_ogr[:-2]
+            for i in range(lw): 
+                  left[i] = wspolczynniki[i]
+
+            #print("\nznak rownania (=, <=, >=)") 
+            if (znak == "="):
+                  ls_eq.append([x for x in left])
+                  rs_eq.append(right)
+            elif (znak == "<="):
+                  ls_eql.append([x for x in left])
+                  rs_eql.append(right)
+            elif (znak == ">="):
+                  ls_eql.append([x * (-1) for x in left])
+                  rs_eql.append(right * (-1))
+            else:
+                  print("nie ma takiego znaku do wyboru")
+
+      for i in range(lw):
+            obj[i] *= -1
+
+      opt = linprog(c=obj, A_ub=ls_eql,  b_ub=rs_eql, A_eq=ls_eq, b_eq=rs_eq, method="revised simplex")
+
+      print("\nCzy optymalizacja zakończyła się sukcesem?:   ", opt.success) #informacja czy optymalizacja przeszla pomyslnie
+      print(opt.x)
+
+      zysk = opt.fun*(-1) #zysk koncowy
+      wynik = opt.success
+      wspolczynniki = opt.x
+
+      print("zysk %.2f zł" %zysk)
+      return zysk, wynik, wspolczynniki
